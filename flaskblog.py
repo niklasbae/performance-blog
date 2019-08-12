d36e513b513f3a9aefe21c3ada9bc4c484aae92c
niklasbae <niklasbae@hotmail.com>
2019-08-12 23:13:28 +0200

    bold test

--- flaskblog.py
+++ flaskblog.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     name = request.args.get("name", "World")
-    return f'<h1>Hello, {escape(name)}!</h1>'
+    return f'<h1>Hello, <b>{escape(name)}!</b></h1>'
