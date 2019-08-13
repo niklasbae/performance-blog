f67833a3921b2980f3729fa4bb84d5138e714591
niklasbae <niklasbae@hotmail.com>
2019-08-13 09:52:37 +0200

    expanded content

--- flaskblog.py
+++ flaskblog.py
@@ -3,6 +3,15 @@ from flask import Flask, escape, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+@app.route('/home')
+def home():
     name = request.args.get("name", "World")
     return f'<h1>Hello, <b>{escape(name)}!</b></h1>'
+
+@app.route('/about')
+def about():
+    return f'<h1>This is the about page!</b></h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
