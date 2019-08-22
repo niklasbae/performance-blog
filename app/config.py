4b988283869dc0f1870e2b88b231ab53e2fee457
niklasbae <niklasbae@hotmail.com>
2019-08-22 09:50:41 +0200

    updated favicon

--- app/config.py
+++ app/config.py
@@ -8,9 +8,10 @@ with open('app/config.json') as config_file:
 
 class Config:
     SECRET_KEY = config.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
+    # SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
     # SECRET_KEY = os.environ.get('SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = '587'
     MAIL_USE_TLS = 'True'
